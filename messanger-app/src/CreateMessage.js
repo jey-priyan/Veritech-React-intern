6a6ae22f6616197fb8d6d55f3113ae2f14b9543d
jey-priyan <jjpriyan19@gmail.com>
2024-02-16 09:16:28 +0530

    messanger

---
+++ messanger-app/src/CreateMessage.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class CreateMessage extends Component {
+    state = {
+        message: '',
+    };
+
+    isDisabled = () => {
+        return this.state.message === '';
+    };
+
+    handleInputChange = (event) => {
+        let value = event.target.value;
+        this.setState({
+            message: value
+        })
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.props.createMessage(this.state.message);
+        this.setState({message: ''});
+    };
+
+    render() {
+
+        return (
+            <div>
+                <form className="input-group" onSubmit={this.handleSubmit}>
+                    <input type="text"
+                           className="form-control"
+                           onChange={this.handleInputChange}
+                           value={this.state.message}
+                           placeholder="Enter your message..."
+                           rows="3" /* Set rows to 3 to allow for multiple lines */
+                        style={{ resize: 'none' }}
+                    />
+                    <div className="input-group-append">
+                        <button
+                            className="btn submit-button"
+                            disabled={this.isDisabled()}
+                        >
+                            SEND
+                        </button>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+CreateMessage.propTypes = {
+    createMessage: PropTypes.func.isRequired,
+};
+
+export default CreateMessage;
